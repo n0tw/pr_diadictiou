473c41399437c4e5323243724315292a2ae7f7bf
n0tw <evgenia.123@hotmail.com>
2024-02-26 12:34:05 +0200

    cookies

--- import requests.py
+++ import requests.py
@@ -10,11 +10,12 @@ def more(text):
             if reply == 'n':
                 break
 
-url = 'http://eclass.upatras.gr/'  
+url = input("Enter a url")
 
 with requests.get(url) as response:  
     html = response.text
-    server = response.headers.get('Server')
-    if server:
-        print("server")
-    more(html)
+    cookies = response.headers.get('Set-Cookie')
+    if cookies:
+        print("The cookie is", cookies)
+    else:
+        print("No cookie found")
